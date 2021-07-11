ad87bfa3c455185bf8188cf20a21852dc3ab79a1
Atsu <kamnknon@gmail.com>
2021-07-11 11:59:17 +0900

    Toolbarを導入

---
+++ front/src/components/Navigation/container.jsx
@@ -0,0 +1,12 @@
+import Navigation from "./presentation";
+import {connect} from "react-redux";
+
+const mapStateToProps = state => ({});
+const mapDispatchProps = dispatch => ({});
+const mergeProps = (stateProps, dispatchProps) => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchProps,
+  mergeProps
+  )(Navigation);
